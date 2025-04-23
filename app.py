0f096d88b80ae519d205ed904c07a3f19868b232
liufufu <91358749+Shang0320@users.noreply.github.com>
2025-04-23 12:35:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_title="嵌入外部網頁", layout="wide")
-
-st.title("嵌入外部網頁展示")
+st.set_page_config(page_title="教測中心圖書室", layout="wide")
 
 st.components.v1.iframe("https://shang0320.github.io/", height=700)
